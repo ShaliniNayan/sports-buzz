3a6ae40dbe8a8b977cbee126b1f956952f7a8d8d
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-10-20 21:13:05 +0530

    Update the backend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,47 @@
-import express from 'express';
+    import express from 'express';
 
-const app = express();
+    const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Server is ready');
-});
+    app.get('/', (req, res) => {
+        res.send('Server is ready');
+    });
 
-const port = process.env.PORT || 3000;
+    // get a list of 5 sports
 
-app.listen(port, () => {
-    console.log(`Server at http://localhost:${port}`);
-    }
-);
+    app.get('/sports', (req, res) => {
+        const sports = [
+            {
+                id: 1,
+                title: 'Badminton',
+                content: 'Badminton page'
+            },
+            {
+                id: 2,
+                title: 'Cricket',
+                content: 'Cricket page'
+            },
+            {
+                id: 3,
+                title: 'Football',
+                content: 'Football page'
+            },
+            {
+                id: 4,
+                title: 'Tennis',
+                content: 'Lawn Tennis page'
+            },
+            {
+                id: 5,
+                title: 'Squash',
+                content: 'Squash page'
+            }
+        ];
+        res.send(sports);
+    });
+
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`Server at http://localhost:${port}`);
+        }
+    );
